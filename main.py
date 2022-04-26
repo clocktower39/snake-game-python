f13fcf4324b086c9662a9cba07f64debbf55d54b
clocktower39 <clocktower39@aol.com>
2022-04-25 21:25:18 -0700

    snake always moves, added body and body movement to snake

--- main.py
+++ main.py
@@ -1,37 +1,59 @@
 import pygame
 from pygame.locals import *
+import time
+
+SIZE = 40
 
 class Snake:
-    def __init__(self, parent_screen):
-        self.parent_screen = parent_screen
+    def __init__(self, surface, length):
+        self.length = length
+        self.parent_screen = surface
         self.block = pygame.image.load("resources/block.jpg").convert()
-        self.x = 100
-        self.y = 100
+        self.x = [SIZE] * length
+        self.y = [SIZE]  * length
+        self.direction = 'down'
 
     def draw(self):
         self.parent_screen.fill((110, 110, 5))
-        self.parent_screen.blit(self.block, (self.x, self.y))
+        for i in range(self.length):
+            self.parent_screen.blit(self.block, (self.x[i], self.y[i]))
         pygame.display.flip()
 
     def move_left(self):
-        self.x -= 10
+        self.direction = 'left'
         self.draw()
     def move_right(self):
-        self.x += 10
+        self.direction = 'right'
         self.draw()
     def move_up(self):
-        self.y -= 10
+        self.direction = 'up'
         self.draw()
     def move_down(self):
-        self.y += 10
+        self.direction = 'down'
+        self.draw()
+
+    def walk(self):
+
+        for i in range(self.length-1, 0, -1):
+            self.x[i] = self.x[i -1]
+            self.y[i] = self.y[i -1]
+
+        if(self.direction) == 'up':
+            self.y[0] -= SIZE
+        if(self.direction) == 'down':
+            self.y[0] += SIZE
+        if(self.direction) == 'left':
+            self.x[0] -= SIZE
+        if(self.direction) == 'right':
+            self.x[0] += SIZE
         self.draw()
 
 class Game: 
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((500, 500))
+        self.surface = pygame.display.set_mode((1000, 800))
         self.surface.fill((110, 110, 5))
-        self.snake = Snake(self.surface)
+        self.snake = Snake(self.surface, 20)
         self.snake.draw()
 
     def run(self):
@@ -54,6 +76,9 @@ class Game:
 
                 elif event.type == QUIT:
                     running = False
+
+            self.snake.walk()
+            time.sleep(0.3)
 
 
 if __name__ == "__main__":
